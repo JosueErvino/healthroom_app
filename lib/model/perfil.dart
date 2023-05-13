64ab9f2cb6dbdcb8c5318d87a8c63b78459f5ffe
Josué Ervino Escher Feldeckircher <josue.feldeckircher@cwi.com.br>
2023-05-13 16:35:42 -0300

    model usuario e perfil

---
+++ lib/model/perfil.dart
@@ -0,0 +1,23 @@
+enum Perfil {
+  aluno,
+  instrutor,
+  nutricionista;
+
+  @override
+  String toString() {
+    String perfil = super.toString().split('.').last;
+    return perfil[0].toUpperCase() + perfil.substring(1);
+  }
+
+  bool isProfissional() {
+    return this != Perfil.aluno;
+  }
+
+  bool isNutricionista() {
+    return this == Perfil.nutricionista;
+  }
+
+  bool isInstrutor() {
+    return this == Perfil.instrutor;
+  }
+}
---
+++ lib/model/usuario.dart
@@ -0,0 +1,12 @@
+import 'package:healthroom_app/model/perfil.dart';
+
+class Usuario {
+  late String uid;
+  late String nome;
+  late String email;
+  late String telefone;
+  late String dataNascimento;
+  late Perfil perfil;
+  late double peso;
+  late double altura;
+}
