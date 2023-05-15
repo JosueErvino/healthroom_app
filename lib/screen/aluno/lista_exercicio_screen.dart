9d71608491f2827dd8ac83bc059cc655b5face67
Josué Ervino Escher Feldeckircher <josue.feldeckircher@cwi.com.br>
2023-05-14 21:31:32 -0300

    exibicao da lista de treinos

--- lib/screen/aluno/lista_exercicio_screen.dart
+++ lib/screen/aluno/lista_exercicio_screen.dart
@@ -14,11 +14,37 @@ class ListaExercicioScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
+    void handleEncerrarTreino() {
+      showDialog(
+          context: context,
+          builder: (BuildContext context) => AlertDialog(
+                title: const Text('Encerrar Treino'),
+                content: const Text('Deseja finalizar o treino?'),
+                actions: <Widget>[
+                  TextButton(
+                    onPressed: () => Navigator.pop(context, false),
+                    child: const Text(
+                      'Não',
+                      style: TextStyle(color: Colors.red),
+                    ),
+                  ),
+                  TextButton(
+                    onPressed: () => Navigator.pop(context, true),
+                    child: const Text(
+                      'Sim',
+                      style: TextStyle(
+                        color: Colors.green,
+                      ),
+                    ),
+                  ),
+                ],
+              ));
+    }
+
     return FutureBuilder(
         future: DatabaseService().getTreinoById(idTreino),
         builder: (context, snapshot) {
           if (snapshot.hasError) {
-            print(snapshot.error.toString());
             Navigator.pop(context);
           }
 
@@ -29,16 +55,41 @@ class ListaExercicioScreen extends StatelessWidget {
                   title: Text(nomeTreino),
                 ),
                 body: ListView.separated(
+                  scrollDirection: Axis.vertical,
+                  shrinkWrap: true,
                   itemCount: exercicios.length,
                   separatorBuilder: (context, index) => const Divider(),
                   itemBuilder: (context, index) {
                     return ListTile(
+                      leading: const Icon(
+                        Icons.fitness_center,
+                      ),
+                      trailing: Container(
+                        width: 40.0,
+                        height: 40.0,
+                        decoration: BoxDecoration(
+                          shape: BoxShape.circle,
+                          border: Border.all(width: 2),
+                          color: Theme.of(context).colorScheme.secondary,
+                        ),
+                        child: Center(
+                          child: Text(
+                            '${exercicios[index].carga} kg',
+                            style: Theme.of(context).textTheme.labelMedium,
+                          ),
+                        ),
+                      ),
+
                       title: Text(exercicios[index].descricao),
+                      subtitle: Text(
+                          '${exercicios[index].repeticoes} x ${exercicios[index].series} - ${exercicios[index].carga} kg'),
+                      // onTap: ,
+                      // enabled: //if o exercicio foi executado
                     );
                   },
                 ),
                 floatingActionButton: FloatingActionButton(
-                  onPressed: () {},
+                  onPressed: handleEncerrarTreino,
                   child: const Icon(
                     Icons.check,
                     color: Colors.black,
