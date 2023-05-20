558d9e83a86fccba3aab3cb85ab10827aa4cd0d4
Josué Ervino Escher Feldeckircher <josue.feldeckircher@cwi.com.br>
2023-05-20 10:56:08 -0300

    digramacao imc

--- lib/screen/aluno/perfil_screen.dart
+++ lib/screen/aluno/perfil_screen.dart
@@ -1,12 +1,57 @@
 import 'package:flutter/material.dart';
+import 'package:healthroom_app/model/usuario.dart';
 
 class PerfilScreen extends StatelessWidget {
-  const PerfilScreen({super.key});
+  final Usuario usuario;
+
+  const PerfilScreen({super.key, required this.usuario});
 
   @override
   Widget build(BuildContext context) {
-    return const Center(
-      child: Text('Perfil'),
+    return Row(
+      children: [
+        SizedBox(
+          width: 200,
+          child: Image.asset(
+            'assets/img/body.png',
+            fit: BoxFit.contain,
+          ),
+        ),
+        Expanded(
+          child: Column(
+            children: [
+              InfoCard(usuario: usuario),
+              Card(
+                child: Text('Peso:'),
+              ),
+            ],
+          ),
+        )
+      ],
+    );
+  }
+}
+
+class InfoCard extends StatelessWidget {
+  const InfoCard({
+    super.key,
+    required this.usuario,
+  });
+
+  final Usuario usuario;
+
+  @override
+  Widget build(BuildContext context) {
+    return Card(
+      child: Padding(
+        padding: EdgeInsets.all(8.0),
+        child: Column(
+          children: [
+            const Text('Altura'),
+            Text(usuario.altura.toString()),
+          ],
+        ),
+      ),
     );
   }
 }
