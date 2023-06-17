94b5931ecda944d399b6ba9fc461e1a1ae1067e3
Josué Ervino Escher Feldeckircher <josue.feldeckircher@cwi.com.br>
2023-06-17 09:58:04 -0300

    organizar dados do aluno em tela

--- lib/screen/instrutor/dados_aluno_screen.dart
+++ lib/screen/instrutor/dados_aluno_screen.dart
@@ -12,40 +12,73 @@ class DadosAlunoScreen extends StatelessWidget {
         title: Text(aluno.nome),
       ),
       // show profile of aluno
-      body: Column(
-        children: [
-          // Avatar
-          Container(
-            margin: const EdgeInsets.all(10),
-            child: const CircleAvatar(
-              radius: 50,
+      body: Center(
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.center,
+          children: [
+            const Title(title: "Dados do Aluno"),
+            // Avatar
+            Container(
+              margin: const EdgeInsets.all(10),
+              child: const CircleAvatar(
+                backgroundColor: Colors.grey,
+                radius: 50,
+                child: Icon(
+                  Icons.person,
+                  color: Colors.white,
+                  size: 50,
+                ),
+              ),
             ),
-          ),
-          // Nome
-          Container(
-            margin: const EdgeInsets.all(10),
-            child: Text(
-              aluno.nome,
-              style: const TextStyle(fontSize: 20),
+            // Nome
+            Container(
+              margin: const EdgeInsets.all(10),
+              child: Text(
+                aluno.nome,
+                style: const TextStyle(fontSize: 20),
+              ),
             ),
-          ),
-          // E-mail
-          Container(
-            margin: const EdgeInsets.all(10),
-            child: Text(
-              aluno.email,
-              style: const TextStyle(fontSize: 20),
+            // E-mail
+            Container(
+              margin: const EdgeInsets.all(10),
+              child: Text(
+                aluno.email,
+                style: const TextStyle(fontSize: 20),
+              ),
             ),
-          ),
-          // Telefone
-          Container(
-            margin: const EdgeInsets.all(10),
-            child: Text(
-              aluno.telefone,
-              style: const TextStyle(fontSize: 20),
+            // Telefone
+            Container(
+              margin: const EdgeInsets.all(10),
+              child: Text(
+                aluno.telefone,
+                style: const TextStyle(fontSize: 20),
+              ),
             ),
-          ),
-        ],
+            const Divider(
+              color: Colors.black,
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+class Title extends StatelessWidget {
+  final String title;
+
+  const Title({
+    super.key,
+    required this.title,
+  });
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      margin: const EdgeInsets.all(10),
+      child: Text(
+        title,
+        style: const TextStyle(fontSize: 30, fontWeight: FontWeight.bold),
       ),
     );
   }
