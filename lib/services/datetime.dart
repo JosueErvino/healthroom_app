112bc9e450d5bc1f293d68605320b4e53281469c
Josué Ervino <jo.feldeck@gmail.com>
2023-06-24 11:57:58 -0300

    metodo para validar data de string

--- lib/services/datetime.dart
+++ lib/services/datetime.dart
@@ -2,4 +2,27 @@ class DateTimeService {
   String formatarData(DateTime data) {
     return '${data.day}/${data.month}/${data.year}';
   }
+
+  static bool validarDataNascimento(String dataNascimento) {
+    if (dataNascimento.isEmpty) {
+      return false;
+    }
+    List<String> data = dataNascimento.split('/');
+    if (data.length != 3) {
+      return false;
+    }
+    int dia = int.parse(data[0]);
+    int mes = int.parse(data[1]);
+    int ano = int.parse(data[2]);
+    if (dia < 1 || dia > 31) {
+      return false;
+    }
+    if (mes < 1 || mes > 12) {
+      return false;
+    }
+    if (ano < 1900 || ano > DateTime.now().year) {
+      return false;
+    }
+    return true;
+  }
 }
